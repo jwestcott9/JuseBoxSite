1d74332341a40d0c20d4ea3aa813979841a0497a
jwestcott9 <jwestcott9@gmail.com>
2019-12-10 12:29:22 -0700

    added vendor registration

---
+++ public/api.js
@@ -0,0 +1,34 @@
+
+let database;
+$(document).ready(function(){
+   
+})
+
+// warning for this kind of call if there are ever two of the same addresses inputed it will throw an error and likely crash the server
+// to fix this issue one will have to make a call on the front end to see if the address already exists in the database... if so 
+// the call should be to update a current address information
+function addNewVendor (vendorInfo){
+    const firebaseConfig = {
+        apiKey: "AIzaSyDaQOoyguuNsFWelJ_Y6MXTpm_vk7Mn6Wg",
+        authDomain: "jusebox-ad0be.firebaseapp.com",
+        databaseURL: "https://jusebox-ad0be.firebaseio.com",
+        projectId: "jusebox-ad0be",
+        storageBucket: "jusebox-ad0be.appspot.com",
+        messagingSenderId: "171439509082",
+        appId: "1:171439509082:web:a7cd50665b50b2b38d5263",
+        measurementId: "G-LS8YGM7CLS"
+      };
+      // Initialize Firebase
+    
+firebase.initializeApp(firebaseConfig);
+database = firebase.database();
+    console.log("ApI call" +vendorInfo.email)
+    database.ref('vendors/' + vendorInfo.address).push(vendorInfo).then(function(){
+        location.reload();
+    })
+
+   
+}
+// Authentication will become more and more neccesary for this company website. 
+
+
---
+++ public/register.js
@@ -0,0 +1,25 @@
+let email = document.getElementById("email").value
+let businessName = document.getElementById("businessName").value ;
+function check (){
+    event.preventDefault()
+
+
+    let email = document.getElementById("email").value
+    let businessName = document.getElementById("businessName").value ;
+    let address = document.getElementById("address").value;
+    let numberOfUnits = document.getElementById('numberOfUnits').value;
+    let description = document.getElementById('description').value;
+
+    let vendorInfo = {};
+
+    vendorInfo.email = email;
+    vendorInfo.businessName = businessName;
+    vendorInfo.address = address;
+    vendorInfo.numberOfUnits = numberOfUnits;
+    vendorInfo.description = description;
+
+    console.log(vendorInfo);
+
+    addNewVendor(vendorInfo)
+}
+
